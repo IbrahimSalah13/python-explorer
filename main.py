43cd6bec6bf201152c364cf26167d4dc7767380e
ibrahim salah <ibrahimsalahzefri75@gmail.com>
2024-11-22 23:56:35 +0100

    modification main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import this
-
-
-def hello_python():
-    zen = this.s.split('\n')
-    print("Hello, Python!")
-    print("Here's the Zen of Python:")
-    for line in zen:
-        print(line)
-
-
-if __name__ == "__main__":
-    hello_python()
+print("Hello world")
