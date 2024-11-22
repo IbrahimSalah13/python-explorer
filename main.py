a86f5f59dcdc9868d72a32097303fc62a4338e90
LahcenEzzara <114707306+LahcenEzzara@users.noreply.github.com>
2024-11-22 23:52:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-print("Hello world")
+import this
 
-def somme(a, b):
-    return a + b
 
-# code
+def hello_python():
+    zen = this.s.split('\n')
+    print("Hello, Python!")
+    print("Here's the Zen of Python:")
+    for line in zen:
+        print(line)
+
+
+if __name__ == "__main__":
+    hello_python()
